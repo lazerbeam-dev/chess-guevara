864f49efe5de6a5f6421f30fd5ecc6336f2d7ae3
Jack Tomlinson <jackiet333@protonmail.com>
2023-01-17 11:51:44 -0600

    reformat logic layout

--- src/AIDisplay.vue
+++ src/AIDisplay.vue
@@ -1,53 +1,49 @@
 <template>
     <div>
-        {{  text }}
-        <div>
-        {{ time }}
     </div>
-    </div>
-    
 </template>
 <script>
 import bus from './bus.js'
 export default {
-  name: 'AIDisplay',
-  data: function(){
-    return {
-        text: "the revolutionary plays the game of the capitalists, so you go first, capitalist pig",
-        taunts: ["I will redistribute your life!", 
-        "you fail to see the bias inherent in the system", 
-        "let us free ourselves together brother", 
-        "why don't we promote all the pawns and have a good time?"
+    name: 'AIDisplay',
+    data: function () {
+        return {
+            showText: true,
+            text: "the revolutionary must establish revolution from within the capitalist system... so it's your turn first, capitalist",
+            taunts: ["I will redistribute your life!",
+                "you fail to see the bias inherent in the system",
+                "let us free ourselves together brother",
+                "why don't we promote all the pawns and have a good time?"
             ],
-        checks: [
-            "check your privelege",
-            "check yourself before you shrek yourself, swamp face",
-            "check... check... ah one two, revolution comin though"
+            checks: [
+                "check your privelege",
+                "check yourself before you shrek yourself, swamp face",
+                "check... check... ah one two, revolution comin though"
             ],
-        losses: [
-            "you play well, capitalist, but revolution is inevitable!"
-        ],
-        wins: ["victory is mine, capitist, now let's play one to decide who gets to rule Canada"],
+            losses: [
+                "you play well, capitalist, but revolution is inevitable!"
+            ],
+            wins: ["victory is mine, capitist, now let's play one to decide who gets to rule Canada"],
 
-        time: null
-    }   
-  },
-  methods: {
-    Taunt(){
-        this.text = this.taunts[Math.floor(Math.random() * this.taunts.length)]
+            seriousOutput: null
+        }
     },
-    ExplainCalculation(moveInfo){
-        
+    methods: {
+        Taunt() {
+            this.text = this.taunts[Math.floor(Math.random() * this.taunts.length)]
+        },
+        ExplainCalculation(moveInfo) {
+
+        },
+        GameEnd() {
+
+        }
     },
-    GameEnd(){
-        
+    created() {
+        bus.$on('AImove', (move, moveInfo) => {
+            console.log("hello", move)
+            this.Taunt()
+        })
     }
-  },
-  created() {
-    bus.$on('AImove', (move, moveInfo) => {
-        console.log("hello", move)
-      this.Taunt()
-    })
-  }
 }
 </script>
--- src/newboard.vue
+++ src/newboard.vue
@@ -5,6 +5,7 @@ import { chessboard, move } from 'vue-chessboard'
 import bus from './bus.js'
 import { Chess } from 'chess.js'
 import AIDisplayVue from './AIDisplay.vue'
+import * as AI from './AILogic'
 
 export default {
   name: 'newboard',
@@ -31,89 +32,13 @@ export default {
         this.aiNextMove()
       };
     },
-    evalBoard(boardstate, maximizingPlayer, moves) {
-      if (boardstate.isCheckmate()) {
-        console.log("checkmate max", maximizingPlayer)
-        return maximizingPlayer ? -1000 : 1000
-      }
-      let fen = boardstate.fen()
-      fen = fen.split(" ")[0]
-      let pawns = (fen.split("p").length - 1) - (fen.split("P").length - 1)
-      let knights = (fen.split("n").length - 1) - (fen.split("N").length - 1)
-      let bishops = (fen.split("b").length - 1) - (fen.split("B").length - 1)
-      let rooks = (fen.split("r").length - 1) - (fen.split("R").length - 1)
-      let queens = (fen.split("q").length - 1) - (fen.split("Q").length - 1)
-      let evaluation = pawns + (3 * knights) + (3 * bishops) + (5 * rooks) + (9 * queens)
-
-      return evaluation
-      //let pawns = fen.split("p").length - 1
-    },
-    minimax(boardstate, depth, maximizingPlayer, alpha, beta) {
-      let availableMoves = boardstate.moves()
-      if (depth == 0 || boardstate.isGameOver()) {
-        return this.evalBoard(boardstate, maximizingPlayer, availableMoves)
-      }
-      if (maximizingPlayer) {
-        let value = -10000
-        for (let i = 0; i < availableMoves.length; i++) {
-          let clonedBoard = new Chess(boardstate.fen())
-          clonedBoard.move(availableMoves[i])
-          let minimax = this.minimax(clonedBoard, depth - 1, false, alpha, beta)
-          value = Math.max(value, minimax)
-          if (value >= beta) {
-            break
-          }
-          alpha = Math.max(alpha, value)
-        }
-        return value
-      }
-      else { //minimizing
-        let value = 10000
-        for (let i = 0; i < availableMoves.length; i++) {
-          let clonedBoard = new Chess(boardstate.fen())
-          clonedBoard.move(availableMoves[i])
-          let minimax = this.minimax(clonedBoard, depth - 1, true, alpha, beta)
-          value = Math.min(value, minimax)
-          if (value <= alpha) {
-            break
-          }
-          beta = Math.min(beta, value)
-        }
-        return value
-      }
-    },
-    calculateBestMove(moves, fen) {
-      var bestSoFar = -10000
-      let moveReturn = null
-      const randomizeBest = true
-      let candidateMoves = []
-
-      for (var i = 0; i < moves.length; i++) {
-        let AIChess = new Chess(fen)
-        AIChess.move(moves[i]);
-        let evaluation = this.minimax(AIChess, 2, false, -10000, 10000)
-        if (evaluation > bestSoFar) {
-          bestSoFar = evaluation
-          moveReturn = moves[i]
-          candidateMoves = []
-          candidateMoves.push(moves[i])
-          //console.log(evaluation, moves[i])
-        }
-        else if (evaluation == bestSoFar) {
-          candidateMoves.push(moves[i])
-        }
-      }
-      if (candidateMoves.length > 1) {
-        console.log("picking from ", candidateMoves.length, " candidatemoves")
-        moveReturn = candidateMoves[candidateMoves.length * Math.random() | 0]
-      }
-      return [moveReturn, "test"]
-    },
     aiNextMove() {
       let moves = this.game.moves()
+
+      bus.$emit('AIHasMoves', moves)
       let fen = this.game.fen()
       console.log(moves)
-      let [bestMove, moveInfo] = this.calculateBestMove(moves, fen)
+      let [bestMove, moveInfo] = AI.calculateBestMove(moves, fen)
       console.log(bus)
       bus.$emit('AImove', [bestMove, moveInfo])
       this.game.move(bestMove)
